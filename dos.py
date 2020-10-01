3962432d710f659a2e4af4ae60a996397d4c970e
Tigran <tigranv1989@gmail.com>
2020-10-02 00:31:03 +0400

    Strong VT

---
+++ dos.py
@@ -0,0 +1,12 @@
+from time import sleep
+print("DDoSer")
+print("Url сайта:")
+x = input()
+print("Количество потоков:")
+input()
+print("Кол-во запросов для каждого потока:")
+w = input()
+
+while True:
+   print("Запрос на " + x + " выполнен")
+   sleep(50 / int(w))
